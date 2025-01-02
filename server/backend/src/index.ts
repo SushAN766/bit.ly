0c6176a828fd30398fe51c9e97bc2b21421a901a
SushAN766 <sushanthbs766@gmail.com>
2025-01-03 01:06:04 +0530

    updated

--- server/backend/src/index.ts
+++ server/backend/src/index.ts
@@ -9,12 +9,10 @@ const prisma = new PrismaClient()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-
 app.get('/urls', async (c) => {
   const urls = await prisma.url.findMany()
   return c.json(urls, 200)
 })
-
 app.post('/urls/create', async (c) => {
   try {
     const { urls } = await c.req.json<{ urls : string }>()
@@ -30,7 +28,6 @@ app.post('/urls/create', async (c) => {
     return c.json({ error: 'Failed to create short URL' }, 500)
   }
 })
-
 app.get("urls/:slug", async (c) => {
   const { slug } = c.req.param();
   const url = await prisma.url.findUnique({
@@ -48,7 +45,6 @@ app.get("urls/:slug", async (c) => {
 
   return c.json(url, 200)
 });
-
 app.delete("urls/:slug", async (c) => {
   const { slug } = c.req.param();
   const url = await prisma.url.delete({
@@ -61,7 +57,6 @@ app.delete("urls/:slug", async (c) => {
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
-
 serve({
   fetch: app.fetch,
   port,
